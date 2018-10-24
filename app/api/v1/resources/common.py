6d9bb8e544427965f7b8c4038e7090c8b069c0dd
a-wakeel <awkhan978@gmail.com>
2018-10-24 10:22:02 +0500

    integrate marshmallow

--- app/api/v1/resources/common.py
+++ app/api/v1/resources/common.py
@@ -1,11 +1,39 @@
 """
 Module for common resources.
 """
+import json
+
+from flask import Response
 from flask_restful import Resource
+from app.api.v1.schemas import CommonResponse
 
 
 class BaseRoutes(Resource):
+    """Base Routes Handlers class."""
 
-    @staticmethod
-    def get():
-        return 'welcome'
+    # noinspection PyMethodMayBeStatic
+    def get(self):
+        """GET method handler for base routes."""
+        # your code here
+        return Response(json.dumps(CommonResponse().dump({'message': 'flask-bp API Version 1.0',
+                                                          'method': 'GET',
+                                                          'route_path': 'api/v1/'}).data),
+                        status=200, mimetype='application/json')
+
+    # noinspection PyMethodMayBeStatic
+    def post(self):
+        """POST method handler for base routes."""
+        # your code here
+        return Response(json.dumps(CommonResponse().dump({'message': 'flask-bp API Version 1.0',
+                                                          'method': 'POST',
+                                                          'route_path': 'api/v1/'}).data),
+                        status=200, mimetype='application/json')
+
+    # noinspection PyMethodMayBeStatic
+    def put(self):
+        """PUT method handler for base routes."""
+        # your code here
+        return Response(json.dumps(CommonResponse().dump({'message': 'flask-bp API Version 1.0',
+                                                          'method': 'PUT',
+                                                          'route_path': 'api/v1/'}).data),
+                        status=200, mimetype='application/json')
--- app/api/v1/schemas/__init__.py
+++ app/api/v1/schemas/__init__.py
@@ -1,3 +1,4 @@
 """
 Top level module for api v1 schemas.
 """
+from app.api.v1.schemas.common import *
