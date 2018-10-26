48faab8e3f8fa02603f2b8d3fecf7ba482c2f7d1
a-wakeel <awkhan978@gmail.com>
2018-10-26 11:40:39 +0500

    implement user resources

--- app/api/v1/resources/user.py
+++ app/api/v1/resources/user.py
@@ -3,10 +3,12 @@ Module for user resource.
 """
 import json
 
+from app import app
 from flask import Response, request
 from flask_restful import Resource
 
 from app.database.models.user import User
+from app.api.v1.schemas.users import UserArgs, UserResponse
 
 
 class Users(Resource):
@@ -16,10 +18,21 @@ class Users(Resource):
         """GET method handler."""
         user_id = request.args.get('user_id')
         user = User()
-        print(user.get(user_id))
-        return Response(json.dumps({'messgae': 'user route GET'}), status=200, mimetype='application/json')
+        return Response(json.dumps(UserResponse().dump(user.get(user_id)).data),
+                        status=200, mimetype='application/json')
 
     def post(self):
         """POST method handler."""
-        print(request.data)
-        return 'abcd'
+        user_data = json.loads(request.data.decode('utf-8'))
+        try:
+            user = User(full_name=user_data.get('full_name'),
+                        user_name=user_data.get('user_name'),
+                        email_id=user_data.get('email_id'))
+            user.add()
+            return Response(json.dumps({'message': 'user {0} added successfully'.format(user_data.get('user_name'))}),
+                            status=200, mimetype='application/json')
+        except Exception as e:
+            app.logger.error('exception encountered')
+            app.logger.exception(e)
+            return Response(json.dumps({'message': 'user {0} addition failed'.format(user_data.get('user_name'))}),
+                            status=500, mimetype='application/json')
