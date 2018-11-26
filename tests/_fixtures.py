5b23ea51854606caba9c3479821ee8f9c8a8a70c
a-wakeel <awkhan978@gmail.com>
2018-11-26 12:06:35 +0500

    add temp db fixture

--- tests/_fixtures.py
+++ tests/_fixtures.py
@@ -3,15 +3,44 @@ fixtures module
 """
 import pytest
 
+from testing.postgresql import Postgresql
 
-@pytest.fixture()
-def flask_app():
+
+@pytest.yield_fixture(scope='session')
+def app():
+    """app fixture"""
+    from app import app as app_
+    app_.testing = True
+    app_.debug = False
+
+    # initialize a temp db instance
+    postgresql = Postgresql()
+    app_.config['SQLALCHEMY_DATABASE_URI'] = postgresql.url()
+    yield app_
+
+    app_.testing = False
+    app_.debug = True
+    postgresql.stop()
+
+
+@pytest.fixture(scope='session')
+def flask_app(app):
     """flask app fixture."""
-    from app import app
-    app.testing = True
-    app.debug = False
     client = app.test_client()
     ctx = app.test_request_context()
     ctx.push()
     yield client
     ctx.pop()
+
+
+@pytest.yield_fixture(scope='session')
+def db(app):
+    """Database fixture"""
+    from app import db
+
+    # assign fixtured app
+    db.app = app
+    db.create_all()
+    yield db
+
+    db.drop_all()
